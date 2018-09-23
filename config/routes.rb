aa9d49086ee792a83ea32c13c5863155f5cdab53
dzagozdzon <zagozdzond@gmail.com>
2018-09-23 15:51:09 +0200

    Add controllers and views

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,25 @@
 Rails.application.routes.draw do
+  get 'categories/new'
+  get 'categories/create'
+  get 'categories/update'
+  get 'categories/edit'
+  get 'categories/destroy'
+  get 'categories/index'
+  get 'categories/show'
+  get 'drinks/new'
+  get 'drinks/create'
+  get 'drinks/update'
+  get 'drinks/edit'
+  get 'drinks/destroy'
+  get 'drinks/index'
+  get 'drinks/show'
+  get 'about/index'
+  get 'about' => 'about#index'
+
+  root 'drinks#index'
+
+  resources :drinks
+  resources :categories
+  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
